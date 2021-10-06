2e939e4ba38d3971da65aaeb8d40d7cd232de4ef
Son518 <baidawardipendar@gmail.com>
2021-10-06 10:08:35 +0100

    complete the tutorial

--- test.py
+++ test.py
@@ -21,20 +21,20 @@ names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv('./test_data.csv', names=names)
 
 # type of dataset
-print(type(dataset))
+# print(type(dataset))
 
 # shape
-print(dataset.shape)
+# print(dataset.shape)
 
 # head
-print(dataset.head(90))
+# print(dataset.head(90))
 
 # descriptions
 # return count, mean, std, min, max
-print(dataset.describe())
+# print(dataset.describe())
 
 # class distribution
-print(dataset.groupby('class').size())
+# print(dataset.groupby('class').size())
 
 # box and whisker plots
 # dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
@@ -45,5 +45,48 @@ print(dataset.groupby('class').size())
 # pyplot.show()
 
 # scatter plot matrix
-scatter_matrix(dataset)
-pyplot.show()
+# scatter_matrix(dataset)
+# pyplot.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation =\
+    train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	# print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms
+# pyplot.boxplot(results, labels=names)
+# pyplot.title('Algorithm Comparison')
+# pyplot.show()
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# print(predictions)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
